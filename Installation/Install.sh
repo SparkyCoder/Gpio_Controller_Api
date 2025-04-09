356e00f7fb661ff2bcd41240927ff9e623592f83
sparkyCoder <dkob8789@gmail.com>
2025-04-09 13:29:26 -0400

    BugFix 1.4.1
    - I updated the code, but not the debian packages with the fix. This commit has those.

--- Installation/Install.sh
+++ Installation/Install.sh
@@ -3,13 +3,22 @@ ARCHITECTURE=$(uname -m)
 case $ARCHITECTURE in
     x86_64)
         echo "64-bit architecture detected."
-        URL="https://github.com/SparkyCoder/Gpio_Controller_Api/raw/refs/heads/main/Installation/linux-x64/gpio-controller-api-1.4.deb"
-             
+        URL="https://github.com/SparkyCoder/Gpio_Controller_Api/raw/refs/heads/main/Installation/linux-x64/gpio-controller-api-1.4.deb" 
         ;;
     aarch64)
         echo "ARM 64-bit architecture detected."
         URL="https://github.com/SparkyCoder/Gpio_Controller_Api/raw/refs/heads/main/Installation/linux-arm64/gpio-controller-api-1.4.deb"
         ;;
+    #armv7l)
+    #    echo "ARM 64-bit architecture detected."
+    #    URL="https://github.com/SparkyCoder/Gpio_Controller_Api/raw/refs/heads/main/Installation/linux-arm/gpio-controller-api-1.4.deb"
+    #    ;;
+    #armhf)
+    #    echo "ARM 64-bit architecture detected."
+    #    URL="https://github.com/SparkyCoder/Gpio_Controller_Api/raw/refs/heads/main/Installation/linux-arm/gpio-controller-api-1.4.deb"
+    #    ;;
+      
+      
     *)
         echo "Unknown architecture: $ARCHITECTURE"
         ;;
