2b7ea7d6d1ed67e4419a2dcaf21db9f3c1a0cad9
David Kobuszewski <SparkyCoder@users.noreply.github.com>
2025-04-09 21:23:57 -0400

    Update Install.sh

--- Installation/Install.sh
+++ Installation/Install.sh
@@ -3,11 +3,11 @@ ARCHITECTURE=$(uname -m)
 case $ARCHITECTURE in
     x86_64)
         echo "64-bit architecture detected."
-        URL="https://github.com/SparkyCoder/PinPanda-API/raw/refs/heads/main/Installation/linux-x64/PinPanda-Api-1.4.deb" 
+        URL="https://github.com/SparkyCoder/PinPanda-API/raw/refs/heads/main/Installation/linux-x64/pinpanda-api-1.4.deb" 
         ;;
     aarch64)
-        echo "ARM 64-bit architecture detected."
-        URL="https://github.com/SparkyCoder/PinPanda-API/raw/refs/heads/main/Installation/linux-arm64/PinPanda-Api-1.4.deb"
+        echo "ARM 64-bit architecture detected." 
+        URL="https://github.com/SparkyCoder/PinPanda-API/raw/refs/heads/main/Installation/linux-arm64/pinpanda-api-1.4.deb"
         ;;
     #armv7l)
     #    echo "ARM 64-bit architecture detected."
